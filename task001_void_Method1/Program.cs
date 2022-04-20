bb15c21a15952cd32060c9cd48f3f42bdb38ddd5
dmitry_di <dnd_d@mail.ru>
2022-04-20 12:53:00 +0300

    Описали четыре варианта функций

---
+++ task001_void_Method1/Program.cs
@@ -0,0 +1,56 @@
+﻿/* Метод 1 Когда функция не принимает значение 
+никаких аргументов и ничего не возвращает*/
+
+/*Console.Clear();
+void Method1()
+{
+    Console.WriteLine("Автор ...");
+}
+Method1();*/
+
+/* Метод 2 Когда функция принимает значения, 
+но ничего не возвращает*/
+/*void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2(msg: "Текстовое сообщение");*/
+
+/*void Method21(string msg, int count)
+{
+    int i=0;
+    while(i<count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+//Method21(msg: "Текстовое сообщение", count: 4);
+Method21(count: 4, msg: "Текстовое сообщение");*/
+
+/* Метод 3 Когда функция не принимает значения, 
+но при этом возвращает занчения аргумента*/
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+Console.WriteLine(year);
+
+/* Метод 4 Когда функция и принимает значения, 
+и при этом возвращает занчения аргумента*/
+
+string Method4(int count, string text)
+{
+    int i=0;
+    string result = String.Empty;
+
+    while(i<count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string res = Method4(10, "I Love logistics! ");
+Console.WriteLine(res);
