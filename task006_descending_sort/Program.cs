00162225c94e877bfab763c35681ab6152b11452
dmitry_di <dnd_d@mail.ru>
2022-04-20 15:24:40 +0300

    Сортировка массива по убыванию

---
+++ task006_descending_sort/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача. Сортировка массива по убыванию
+
+Console.Clear();
+int[] array = {1, 5, 4, 3, 2, 6, 7};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i<count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+        for (int i=0; i<array.Length - 1 ; i++)
+        {
+            int maxPosition = i;
+
+            for (int j=i+1; j< array.Length; j++)
+            {
+                if(array[j]> array[maxPosition]) maxPosition = j;
+            }
+
+            int temporary = array[i];
+            array[i] = array[maxPosition];
+            array[maxPosition] = temporary;            
+        }
+}
+PrintArray(array);
+SelectionSort(array);
+PrintArray(array);
+
+
+
